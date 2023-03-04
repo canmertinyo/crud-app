e849f1e958e64509b055dbc87f59b134e6f55cad
CANMERTINYO <38213551+canmertinyo@users.noreply.github.com>
2023-03-04 16:26:08 +0300

    add middlewares
    added compression
    added cookieparser
    added morgan
    added cors

--- app.js
+++ app.js
@@ -3,11 +3,19 @@ const tasks = require('./routes/tasks');
 const app = express();
 const connect = require('./database/connect');
 const helmet = require('helmet');
+const compression = require('compression');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const cors = require('cors');
 
 async function bootstrap() {
   //middleware
   app.use(express.json());
   app.use(helmet());
+  app.use(compression());
+  app.use(cookieParser());
+  app.use(morgan());
+  app.use(cors());
 
   //routes
   app.get('/hello', (req, res) => {
