e348bad10f019af5aa3516204bf1ab3cd8d5cfb7
CANMERTINYO <38213551+canmertinyo@users.noreply.github.com>
2023-03-04 19:20:59 +0300

    add more CRUD operations

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -35,8 +35,22 @@ const updateTask = (req, res) => {
   res.send('update task');
 };
 
-const deleteTask = (req, res) => {
-  res.send('delete task');
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const deleteTask = await Task.findOneAndDelete({ _id: taskID });
+    if (!deleteTask) {
+      return res.status(404).json({ msg: 'no task with id: ' + taskID });
+    }
+    res.status(200).json({
+      info: {
+        item: deleteTask,
+        message: 'deleted item with no error!',
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({ msg: error });
+  }
 };
 
 module.exports = {
